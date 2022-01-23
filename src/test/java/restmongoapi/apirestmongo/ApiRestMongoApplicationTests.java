bcf970ecc92800862e8e2769a0e07b946b7ae8d5
Pedro-lms <pedrodaluzmachado@gmail.com>
2022-01-22 22:54:26 -0300

    Initial commit

---
+++ src/test/java/restmongoapi/apirestmongo/ApiRestMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package restmongoapi.apirestmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestMongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
